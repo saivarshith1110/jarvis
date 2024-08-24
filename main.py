91571a76c791867668e8dcbaa87f1b7ea98989d3
saivarshith1110 <saivarshith410@gmail.com>
2024-08-24 19:21:16 +0530

    changes

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
 import eel
 
-eel.init("www")
+from engine.features import *
+from engine.command import *
 
-os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+def start():
 
-eel.start('index.html',mode=None,host='localhost',Block=True)
+    eel.init("www")
+
+
+    playAssistantSound()
+
+
+    os.system('start msedge.exe --app="http://localhost:8000/index.html"')
+
+    eel.start('index.html',mode=None,host='localhost',Block=True)
 
